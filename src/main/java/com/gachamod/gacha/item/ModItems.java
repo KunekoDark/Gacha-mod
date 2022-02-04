d357fa73a713d41daea0e6ca7ae3af2c06280dcf
jzells09 <justinzeller2004@gmail.com>
2022-02-03 21:24:25 -0600

    more items

--- src/main/java/com/gachamod/gacha/item/ModItems.java
+++ src/main/java/com/gachamod/gacha/item/ModItems.java
@@ -27,6 +27,25 @@ public class ModItems {
     public static final RegistryObject<TicketDropLegend> LEGEND_TICKET = ITEMS.register("legend_ticket",
             () -> new TicketDropLegend(new Item.Properties().maxStackSize(1).group(ModItemGroup.GACHA_GROUP)));
 
+    //Normal Items
+
+    public static final RegistryObject<Item> CANNON_BASE = ITEMS.register("cannon_base",
+            () -> new Item(new Item.Properties().maxStackSize(1).group(ModItemGroup.GACHA_GROUP)));
+
+    public static final RegistryObject<Item> CANNON_SHAFT = ITEMS.register("cannon_shaft",
+            () -> new Item(new Item.Properties().maxStackSize(1).group(ModItemGroup.GACHA_GROUP)));
+
+    public static final RegistryObject<Item> CANNON_POWER_MODULE = ITEMS.register("cannon_charge_module",
+            () -> new Item(new Item.Properties().maxStackSize(1).group(ModItemGroup.GACHA_GROUP)));
+
+    public static final RegistryObject<Item> CANNON_COMPONENT = ITEMS.register("cannon_component",
+            () -> new Item(new Item.Properties().maxStackSize(1).group(ModItemGroup.GACHA_GROUP)));
+
+    public static final RegistryObject<Item> CANNON_HANDHELD_MODULE = ITEMS.register("cannon_handheld_module",
+            () -> new Item(new Item.Properties().maxStackSize(1).group(ModItemGroup.GACHA_GROUP)));
+
+
+
 
 
     public static void register(IEventBus eventbus){
